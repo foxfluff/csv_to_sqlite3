c9f413698bb3ad5913b1b2dc1ff17fc079397d80
foxfluff <foxfluff@users.noreply.github.com>
2017-02-18 16:34:49 -0500

    Initial Commit. Implemented read_csv

---
+++ csv_to_sqlite3.py
@@ -0,0 +1,31 @@
+
+import argparse
+import sqlite3
+import csv
+
+def read_csv(filename, **kwargs):
+    """read_csv(file, **kwargs) -> dict("header" : [rows], ...)
+    Reads a CSV document and returns a dictionary object where keys are table
+    headers, and the values for it are an array of strings.
+    
+    kwargs are directly passed to csv.DictReader, see help(csv.DictReader)
+    for more info.
+    """
+
+    csv_contents = {}
+
+    with open(filename, "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file, **kwargs)
+
+        for entry in csv_reader:
+            for column in entry:
+                if not column in csv_contents:
+                    csv_contents[column] = [entry[column]]
+                else:
+                    csv_contents[column].append(entry[column])
+
+    return csv_contents
+
+
+if __name__ == "__main__":
+    pass

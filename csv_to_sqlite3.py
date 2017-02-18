42c989f320ecc92f59c1f94eb6ea279402566106
foxfluff <foxfluff@users.noreply.github.com>
2017-02-18 17:02:38 -0500

    Changed read_csv inputs to use file handles instead of string filenames

--- csv_to_sqlite3.py
+++ csv_to_sqlite3.py
@@ -3,26 +3,26 @@ import argparse
 import sqlite3
 import csv
 
-def read_csv(filename, **kwargs):
+def read_csv(csv_file, **kwargs):
     """read_csv(file, **kwargs) -> dict("header" : [rows], ...)
     Reads a CSV document and returns a dictionary object where keys are table
     headers, and the values for it are an array of strings.
-    
+
+    fileobj must have mode = "r"
+
     kwargs are directly passed to csv.DictReader, see help(csv.DictReader)
     for more info.
     """
 
     csv_contents = {}
+    csv_reader = csv.DictReader(csv_file, **kwargs)
 
-    with open(filename, "r") as csv_file:
-        csv_reader = csv.DictReader(csv_file, **kwargs)
-
-        for entry in csv_reader:
-            for column in entry:
-                if not column in csv_contents:
-                    csv_contents[column] = [entry[column]]
-                else:
-                    csv_contents[column].append(entry[column])
+    for entry in csv_reader:
+        for column in entry:
+            if not column in csv_contents:
+                csv_contents[column] = [entry[column]]
+            else:
+                csv_contents[column].append(entry[column])
 
     return csv_contents
 
